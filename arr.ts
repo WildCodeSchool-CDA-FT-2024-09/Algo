8b63a15e1dbc8d7af75cc4b67a7b208d5540d65a
Julien RICHARD <julien.y.richard@gmail.com>
2024-09-30 16:30:20 +0200

    Mise en place de la méthode push avec test unitaire

---
+++ arr.ts
@@ -0,0 +1,18 @@
+class Arr {
+  value: string[] = [];
+  length: number = 0;
+
+
+  push(el :string): number {
+    const array = new Array(this.value.length + 1);
+
+    for (let i = 0; i < this.value.length; i++) {
+      array[i] = this.value[i]
+    }
+    array[this.value.length] = el;
+    this.value = array;
+    return this.value.length;
+  }
+}
+
+export default Arr;
---
+++ test/array.test.ts
@@ -0,0 +1,19 @@
+import Arr from "../arr";
+
+
+describe("Array", () => {
+  it("should create an array", () => {
+    const newArray = new Arr();
+
+    expect(newArray.value).toHaveLength(0);
+  });
+
+  it('should had an element in the array', () => {
+    const newArray = new Arr();
+    const result = newArray.push("a");
+    console.log(newArray.value)
+
+    expect(newArray.value).toHaveLength(1);
+    expect(result).toEqual(expect.any(Number));
+  })
+});
