7104851447a0da79987b4a273be2b0e56ca9102f
Julien RICHARD <julien.y.richard@gmail.com>
2024-10-07 15:24:22 +0200

    Mis en place de la méthode filter

--- arr.ts
+++ arr.ts
@@ -2,16 +2,33 @@ class Arr {
   value: string[] = [];
   length: number = 0;
 
-
-  push(el :string): number {
+  push(el: string): number {
     const array = new Array(this.value.length + 1);
 
     for (let i = 0; i < this.value.length; i++) {
-      array[i] = this.value[i]
+      array[i] = this.value[i];
     }
     array[this.value.length] = el;
     this.value = array;
     return this.value.length;
+  }
+
+  filter(cb: Function): Array<string> {
+    let longueur = 0;
+    for (let i: number = 0; i < this.value.length; i++) {
+      if (cb(this.value[i])) {
+        longueur++;
+      }
+    }
+    const result = new Array(longueur);
+    let j = 0;
+    for (let i = 0; i < this.value.length; i++) {
+      if (cb(this.value[i])) {
+        result[j] = this.value[i];
+        j++;
+      }
+    }
+    return result;
   }
 }
 
--- test/array.test.ts
+++ test/array.test.ts
@@ -1,6 +1,5 @@
 import Arr from "../arr";
 
-
 describe("Array", () => {
   it("should create an array", () => {
     const newArray = new Arr();
@@ -8,12 +7,24 @@ describe("Array", () => {
     expect(newArray.value).toHaveLength(0);
   });
 
-  it('should had an element in the array', () => {
+  it("should had an element in the array", () => {
     const newArray = new Arr();
     const result = newArray.push("a");
-    console.log(newArray.value)
 
+    console.log(result);
     expect(newArray.value).toHaveLength(1);
     expect(result).toEqual(expect.any(Number));
-  })
+  });
+
+  it("should filter element on the array", () => {
+    const newArray = new Arr();
+    newArray.push("ab");
+    newArray.push("abc");
+    newArray.push("abcd");
+    newArray.push("abcde");
+
+    const result = newArray.filter((el: string) => el.length > 2);
+
+    expect(result).toHaveLength(3);
+  });
 });
